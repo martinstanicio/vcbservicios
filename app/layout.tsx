22a0f29b9bf5d8c24b379621e1d9ec16949fd569
Martín Stanicio <martuytimo@gmail.com>
2026-06-14 20:35:39 +0000

    feat: remove theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import "./globals.css";
 import { Roboto_Slab } from "next/font/google";
-import { ThemeProvider } from "next-themes";
 import { cn } from "@/lib/utils";
 
 export { metadata } from "@/app/metadata";
@@ -17,14 +16,7 @@ export default function RootLayout({ children }: Props) {
       suppressHydrationWarning
     >
       <body className="bg-background font-serif text-foreground">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          disableTransitionOnChange
-          enableSystem
-        >
-          {children}
-        </ThemeProvider>
+        {children}
       </body>
     </html>
   );
